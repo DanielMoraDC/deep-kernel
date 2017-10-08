5f4c4a2fdff2423aa2fc58c96f7a4eb160dc9fcc
walle <daniel.mora.checa@gmail.com>
2017-10-08 15:50:20 +0200

    Hidden units as parameters

--- layout.py
+++ layout.py
@@ -4,17 +4,19 @@ from kernels import KernelFunction
 
 
 def example_layout_fn(x, outputs, **params):
+    hidden_units = params.get('hidden_units', 128)
     inputs = _input_layer(x, **params)
-    hidden = _fully_connected(inputs, 128)
+    hidden = _fully_connected(inputs, hidden_units)
     return _fully_connected(hidden, outputs)
 
 
 def kernel_example_layout_fn(x, outputs, **params):
+    hidden_units = params.get('hidden_units', 128)
     inputs = _input_layer(x, **params)
     kernel = KernelFunction()
     # TODO: test whether we should use relu or no activation
-    hidden = _fully_connected(inputs, 128, activation_fn=None)
-    hidden_kernel = kernel.apply_kernel(hidden, dims=128)
+    hidden = _fully_connected(inputs, hidden_units, activation_fn=None)
+    hidden_kernel = kernel.apply_kernel(hidden, dims=hidden_units)
     return _fully_connected(hidden_kernel, outputs, activation_fn=None)
 
 
