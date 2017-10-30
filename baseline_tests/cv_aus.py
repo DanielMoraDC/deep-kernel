5e32dd85d633d0cefe4dbe3c625cdbd8daa1fdde
walle <daniel.mora.checa@gmail.com>
2017-10-31 00:11:37 +0100

    Changed grid for australian CV

--- baseline_tests/cv_aus.py
+++ baseline_tests/cv_aus.py
@@ -4,20 +4,20 @@ import numpy as np
 from protodata import datasets
 from model_validation import evaluate_model_cv
 
-CV_TRIALS = 25
+CV_TRIALS = 20
 SIM_RUNS = 10
 MAX_EPOCHS = 10000
 
 if __name__ == '__main__':
 
     search_space = {
-        'batch_size': hp.choice('batch_size', [16, 32, 64]),
+        'batch_size': hp.choice('batch_size', [16, 32]),
         # l1 ratio not present in paper
         'l2_ratio': hp.choice('l2_ratio', [1e-2, 1e-3, 1e-4]),
         'lr': hp.choice('lr', [1e-3, 1e-4, 1e-5]),
-        'kernel_size': hp.choice('kernel_size', [64, 128, 256]),
+        'kernel_size': hp.choice('kernel_size', [32, 64]),
         'kernel_std': hp.choice('kernel_std', [1e-2, 0.1, 0.25, 0.5, 1.0]),
-        'hidden_units': hp.choice('hidden_units', [64, 128, 256])
+        'hidden_units': hp.choice('hidden_units', [32, 64])
     }
 
     # Fixed parameters
