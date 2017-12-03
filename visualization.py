1fb61d6e7247840d8b8b17510f2be15187f02397
walle <daniel.mora.checa@gmail.com>
2017-12-03 15:42:46 +0100

    Minor change

--- visualization.py
+++ visualization.py
@@ -13,7 +13,7 @@ def write_epoch(writer, run_status, epoch):
     summary = tf.Summary(
         value=[
             tf.Summary.Value(tag='loss', simple_value=run_status.loss()),
-            tf.Summary.Value(tag='error', simple_value=1-run_status.acc()),
+            tf.Summary.Value(tag='error', simple_value=run_status.error()),
             tf.Summary.Value(tag='L2 term', simple_value=run_status.l2())
         ]
     )
